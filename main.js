5dbbf73711df066eb05fd846acbf1ab186a8e0f3
hassanmirza <talk2mirza@gmail.com>
2017-06-01 02:10:57 +0500

    sec

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,ActivityIndicator } from 'react-native';
+import {fetchMeetups} from './constants/api';
 class App extends React.Component {
+static defaultProps ={
+  fetchMeetups
+}
+state ={
+  loading :false,
+  meetups:[]
+}
+async componentDidMount(){
+  this.setState({loading:true});
+  const data = await this.props.fetchMeetups;
+  setTimeout(this.setState({loading:false,meetups:data.meetups}),2000);
+}
+  
   render() {
+      if(this.state.loading){
+      return(
+        <View style={styles.container}>
+          <ActivityIndicator size="large"/>
+        </View>
+      )
+    }
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Text>MeetUp mobile client!</Text>
+        {this.state.meetups.map((meetup ,i) =>(
+          <Text key={i}>{meetup.title}</Text>
+        ))}
       </View>
     );
   }
